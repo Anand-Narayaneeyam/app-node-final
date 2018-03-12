ae921807a0eee03607232b85114b6a735a732734
Anand R <anand.r@esoft.com>
2018-03-12 16:36:23 +0530

    2nd commit

--- app.js
+++ app.js
@@ -32,13 +32,11 @@ app.get('*',(req,res)=>{
     res.sendfile(path.join(__dirname,'public/index.html'));
 })
 
-// app.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
 
-//     res.sendFile(__dirname+ '/public/index.html');
+   res.send('Home Root');
 
-    
-
-// })
+})
 
 app.post('/signup', (req, res) => {
     debugger
